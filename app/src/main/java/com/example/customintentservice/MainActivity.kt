29641dc6dd806cd519c9267f308fc8da05ecb119
Kotik <k-work31@yandex.ru>
2022-08-21 14:30:25 +0300

    lesson9. Сделали заготовки шаблонов для дальнейшей работы

--- app/src/main/java/com/example/customintentservice/MainActivity.kt
+++ app/src/main/java/com/example/customintentservice/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.customintentservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.customintentservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnShowToast.setOnClickListener {
+
+        }
+
+        binding.btnShowSystemService.setOnClickListener {
+
+        }
+
+        binding.btnShowCustomService.setOnClickListener {
+
+        }
     }
 }
